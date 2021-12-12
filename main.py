e08d0cb8e8cf704a0a84d316020b05389b15378a
zchoi22 <88288951+zchoi22@users.noreply.github.com>
2021-12-12 16:36:34 -0700

    implemented basic gui, will add more features during development

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from stock import stock
 from screener import screener as sc
+from tkinter import *
 
-def test():
-    screener = sc()
-    screener.set_screener('hi', ['hi', 12, 13], ['hi2', 13, 14])
-    print(screener.get_screener('hi'))
-    print(screener.format_screener('hi'))
+def donothing():
+   filewin = Toplevel(root)
+   button = Button(filewin, text="Do nothing button")
+   button.pack()
 
 if __name__ == '__main__':
-    test()
+    #configuring gui window with tkinter
+    root = Tk()
+    root.configure(background = 'white')
+    root.title('Rapstar')
+    root.geometry('500x450')
+
+    #feature for gui
+    title = Label(root, text = "Welcome to Rapstar!")
+    title.pack()
+
+    mb = Menu(root)
+    filemenu = Menu(mb, tearoff = 0)
+    filemenu.add_command(label = 'Screener', command=donothing)
+    filemenu.add_separator()
+    filemenu.add_command(label = 'Exit', command = root.quit)
+    mb.add_cascade(label = 'File', menu = filemenu)
+
+    root.config(menu=mb)
+    root.mainloop()
