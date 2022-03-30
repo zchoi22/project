5ee644427fb7eb23324251a546eed8b4ad52d026
zchoi22 <88288951+zchoi22@users.noreply.github.com>
2022-03-29 21:40:46 -0600

    version 0.0 of neural network, bugs with data normalization

--- project/model.py
+++ project/model.py
@@ -16,19 +16,66 @@ def main():
     y_data = []
     scaler = preprocessing.StandardScaler()
 
+    print("==== Retrieving Data =====")
     for ticker in tickers:
         b = stock(ticker)
         data = b.get_data()
-        company_x_data = []
-        company_y_data = []
         for i in range(len(data)):
-            company_x_data.append(data['Open'])
-            company_y_data.append(data['Adj Close'])
+            x_data.append(data['Open'])
+            y_data.append(data['Adj Close'])
 
+    print(x_data[:10], y_data[:10])
+
+    x_data = scaler.fit_transform(np.array(x_data))
+    y_data = scaler.fit_transform(np.array(y_data).reshape(-1,1).flatten())
+
+    print('===== Creating Training/Testing Sets ====')
     x_train = []
     x_test = []
     y_train = []
     y_test = []
+
+    for i in range(len(x_data)):
+        if i&6:
+            x_train.append(x_data[i])
+            y_train.append(y_data[i])
+        else:
+            x_test.append(x_data[i])
+            y_test.append(y_data[i])
+
+    print('==== Normalizing Data ====')
+    x_test = np.array(x_test)
+    y_test = np.array(y_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    x_train = np.array(x_train)
+    y_train(np.array(y_train))
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    print('==== Training Model ====')
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(6, 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1, activation='relu'))
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    model.fit(x_train, y_train, batch_size=32, epochs=1, validation_data=(x_test, y_test))
+
+    score = model.evaluate(x_test, y_test, verbose='VERBOSE')
+    print('Test accuracy: ', score)
+
+    print('==== Saving Model =====')
+    model_json = model.to_json()
+    with open('..\\program\\model.json', 'w') as json_file:
+        json_file.write(model.json)
+    model.save_weights('model.h5')
+
 
 if __name__ == '__main__':
     main()
