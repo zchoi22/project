cb5d8c05fb11c939d3e5bd6b205fd0d068bd9d62
zchoi22 <88288951+zchoi22@users.noreply.github.com>
2022-03-30 13:40:34 -0600

    heavy changes to model, v.1

--- project/model.py
+++ project/model.py
@@ -9,52 +9,74 @@ import csv
 from sklearn import preprocessing
 from yahoo_fin.stock_info import tickers_sp500
 
+#where input is a 1D np.array and returns a 1D np.array
+def normalize_data(input):
+    scaler = preprocessing.MinMaxScaler()
+    return scaler.fit_transform(input.reshape(-1,1)).flatten()
+
+#need to create batches for time steps, 20 time steps, 2 features
+def generate_batches(x_data, y_data, *args):
+    if args == ():
+        while x_data.shape[0]%20 != 0:
+            x_data = np.delete(x_data, -1, axis=0)
+            y_data = np.delete(y_data, -1, axis=0)
+        return np.array(np.vsplit(x_data, 20)), np.array(np.split(y_data, 20))
+    else:
+        while x_data.shape[0]%args[0] != 0:
+            x_data = np.delete(x_data, -1, axis=0)
+            y_data = np.delete(y_data, -1, axis=0)
+        return np.vsplit(x_data, args[0]), np.split(y_data, args[0])
+
+
 def main():
     tickers = tickers_sp500()
 
-    x_data = []
-    y_data = []
-    scaler = preprocessing.StandardScaler()
+    x_train = np.array([[],[]])
+    x_test = np.array([[],[]])
+    y_train = np.array([])
+    y_test = np.array([])
 
     print("==== Retrieving Data =====")
+    seed = 0
     for ticker in tickers:
         b = stock(ticker)
         data = b.get_data()
-        for i in range(len(data)):
-            x_data.append(data['Open'])
-            y_data.append(data['Adj Close'])
+        opens = normalize_data(data['Open'].to_numpy())
+        highs = normalize_data(data['High'].to_numpy())
+        adj_closes = normalize_data(data['Adj Close'].to_numpy())
 
-    print(x_data[:10], y_data[:10])
-
-    x_data = scaler.fit_transform(np.array(x_data))
-    y_data = scaler.fit_transform(np.array(y_data).reshape(-1,1).flatten())
-
-    print('===== Creating Training/Testing Sets ====')
-    x_train = []
-    x_test = []
-    y_train = []
-    y_test = []
-
-    for i in range(len(x_data)):
-        if i&6:
-            x_train.append(x_data[i])
-            y_train.append(y_data[i])
+        if seed%6 == 0:
+            x_test = np.concatenate((x_test, np.stack([opens, highs])), axis=1)
+            y_test = np.concatenate((y_test, adj_closes))
         else:
-            x_test.append(x_data[i])
-            y_test.append(y_data[i])
+            x_train = np.concatenate((x_train, np.stack([opens, highs])), axis=1)
+            y_train = np.concatenate((y_train, adj_closes))
 
-    print('==== Normalizing Data ====')
-    x_test = np.array(x_test)
-    y_test = np.array(y_test)
-    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+        seed+=1
 
-    x_train = np.array(x_train)
-    y_train(np.array(y_train))
-    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+    x_train = x_train.T
+    x_test = x_test.T
 
+    print(x_train.shape, x_test.shape)
+    print(y_train.shape, y_test.shape)
+
+    print('===== Reformatting Arrays ====')
+    x_test, y_test = generate_batches(x_test, y_test)
+    x_train, y_train = generate_batches(x_train, y_train)
+
+    x_test = x_test.reshape((x_test.shape[1], x_test.shape[0], 2))
+    y_test = y_test.T
+
+    x_train = x_train.reshape((x_train.shape[1], x_train.shape[0], 2))
+    y_train = y_train.T
+
+    print(x_train.shape, x_test.shape)
+    print(y_train.shape, y_test.shape)
+
+    #timesteps, inputs so should be x, 2
     print('==== Training Model ====')
     model = Sequential()
-    model.add(LSTM(units=50, return_sequences=True, input_shape=(6, 1)))
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 2)))
     model.add(Dropout(0.2))
     model.add(LSTM(units=50, return_sequences=True))
     model.add(Dropout(0.2))
@@ -65,15 +87,15 @@ def main():
     model.add(Dense(units=1, activation='relu'))
     model.compile(optimizer='adam', loss='mean_squared_error')
 
-    model.fit(x_train, y_train, batch_size=32, epochs=1, validation_data=(x_test, y_test))
+    model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_test, y_test))
 
-    score = model.evaluate(x_test, y_test, verbose='VERBOSE')
+    score = model.evaluate(x_test, y_test, verbose=0)
     print('Test accuracy: ', score)
 
     print('==== Saving Model =====')
     model_json = model.to_json()
-    with open('..\\program\\model.json', 'w') as json_file:
-        json_file.write(model.json)
+    with open('..\\project\\program\\model.json', 'w') as json_file:
+        json_file.write(model_json)
     model.save_weights('model.h5')
 
 
