6f2743b327b9fdc2f1d98bd79efd3ab5244609fd
zchoi22 <88288951+zchoi22@users.noreply.github.com>
2022-05-11 21:08:44 -0600

    major changes to neural network implemented

--- project/main/neural_network/model.py
+++ project/main/neural_network/model.py
@@ -10,29 +10,22 @@ from yahoo_fin.stock_info import tickers_sp500
 import joblib
 
 #where input is a 1D np.array and returns a 1D np.array
-def normalize_data(data, timesteps=20, save_scaler=False):
-    scaler = preprocessing.MaxAbsScaler()
+def normalize_data(data, filename='', save_scaler=False):
+    scaler = preprocessing.MinMaxScaler()
     scaler.fit(data.reshape(-1,1))
     if save_scaler:
-        joblib.dump(scaler, 'scaler.gz')
-    while data.shape[0]%timesteps != 0:
-        data = np.delete(data, -1, axis=0)
+        filename = 'models\\scalers\\' + filename + "_scaler.gz"
+        joblib.dump(scaler, filename)
     return scaler.transform(data.reshape(-1,1)).flatten()
 
 #need to create batches for time steps, 20 time steps, 2 features
-def generate_batches(data, dimensions=1, split=True, timesteps=20):
-    while data.shape[0]%timesteps != 0:
-        data = np.delete(data, -1, axis=0)
-    if dimensions==1 and split:
-        return np.array(np.split(data, timesteps))
-    elif dimensions==2 and split:
-        return np.array(np.vsplit(data, timesteps))
-    elif dimensions==1 and not split:
-        return data
+def generate_batches(data, timesteps=20):
+    return np.array(np.split(data, timesteps))
 
 def main():
-    tickers = tickers_sp500()
+    tickers = tickers_sp500()[0]
     features = 2
+    timesteps = 20
 
     x_data = np.array([[] for i in range(features)])
     y_data = np.array([])
@@ -40,56 +33,37 @@ def main():
     print("==== Retrieving Data =====")
     for ticker in tickers:
         data = stock(ticker, '..\\..\\..\\project\\main\\stock\\historical_data\\').get_data()
-        opens = data['Open'].to_numpy()
-        highs = data['High'].to_numpy()
+        features_x = [data['Open'].to_numpy(), data['High'].to_numpy()]
         adj_closes = data['Adj Close'].to_numpy()
 
-        x_data = np.concatenate((x_data, np.stack([opens, highs])), axis=1)
+        #making sure we can batch the data later, reduces size of the data to %20
+        while adj_closes.shape[0] % timesteps != 0:
+            adj_closes = np.delete(adj_closes, -1)
+            for i in range(len(features_x)):
+                features_x[i] = np.delete(features_x[1], -1)
+
+        x_data = np.concatenate((x_data, np.stack(features_x)), axis=1)
         y_data = np.concatenate((y_data, adj_closes))
+
+    print("==== Normalizing Data ====")
+    features_x = np.split(x_data.T, features, axis=1)
+    for i in range(len(features_x)):
+        features_x[i] = normalize_data(features_x[i], save_scaler=True, filename='x' + str(i))
+    y_data = normalize_data(y_data, save_scaler=True, filename='y')
+    x_data = np.vstack(features_x).T
+
+    print('===== Reformatting Arrays ====')
+    x_data, y_data = generate_batches(x_data, timesteps=timesteps), generate_batches(y_data, timesteps=timesteps)
+
+    x_data = x_data.reshape((x_data.shape[1], x_data.shape[0], 2))
+    y_data = y_data.T
 
     print(x_data.shape, y_data.shape)
 
-    print("==== Normalizing Data ====")
-    feature1, feature2 = np.split(x_data.T, 2, axis=1)
-    feature1, feature2 = generate_batches(feature1, split=False), generate_batches(feature2, split=False)
-    y_data = generate_batches(y_data, split=False)
-
-    feature1, feature2 = normalize_data(feature1).tolist(), normalize_data(feature2).tolist()
-    y_data = normalize_data(y_data).tolist()
-
     print('==== Training and Testing=====')
-    x_train = np.array([[],[]])
-    x_test = np.array([[],[]])
-    y_train = np.array([])
-    y_test = np.array([])
-
-    for i in range(int(len(y_data)/20)):
-        if i%6 != 0:
-            x_train = np.concatenate((x_train, np.stack([np.array(feature1[i:i*20-1]), np.array(feature2[i:i*20-1])])), axis=1)
-            y_train = np.concatenate((y_train, np.array(y_data[i:i*20-1])))
-        else:
-            x_test = np.concatenate((x_test, np.stack([np.array(feature1[i:i*20-1]), np.array(feature2[i:i*20-1])])), axis=1)
-            y_test = np.concatenate((y_test, np.array(y_data[i:i*20-1])))
-
-
-    x_train = x_train.T
-    x_test = x_test.T
-
-    print(x_train.shape, x_test.shape)
-    print(y_train.shape, y_test.shape)
-
-    print('===== Reformatting Arrays ====')
-    x_test, y_test = generate_batches(x_test, y_test)
-    x_train, y_train = generate_batches(x_train, y_train)
-
-    x_test = x_test.reshape((x_test.shape[1], x_test.shape[0], 2))
-    y_test = y_test.T
-
-    x_train = x_train.reshape((x_train.shape[1], x_train.shape[0], 2))
-    y_train = y_train.T
-
-    print(x_train.shape, x_test.shape)
-    print(y_train.shape, y_test.shape)
+    size = x_data.shape[0] - int(x_data.shape[0]/6)
+    x_train, x_test = x_data[:size, :], x_data[size:, :]
+    y_train, y_test = y_data[:size, :], y_data[size:, :]
 
     #timesteps, inputs so should be x, 2
     print('==== Training Model ====')
@@ -102,10 +76,11 @@ def main():
     model.add(Dropout(0.1))
     model.add(LSTM(units=50))
     model.add(Dropout(0.1))
+    model.add(Dense(256, activation='relu'))
     model.add(Dense(units=1, activation='relu'))
     model.compile(optimizer='adam', loss='mean_squared_error')
 
-    model.fit(x_train, y_train, batch_size=32, epochs=6, validation_data=(x_test, y_test))
+    model.fit(x_train, y_train, batch_size=32, epochs=10, validation_data=(x_test, y_test))
 
     score = model.evaluate(x_test, y_test, verbose=0)
     print('Test accuracy: ', score)
--- project/main/neural_network/prediction.py
+++ project/main/neural_network/prediction.py
@@ -1,24 +1,30 @@
 from keras.models import load_model
 from main.stock.stock import stock
 import numpy as np
-from main.neural_network.model import normalize_data
 import joblib
 
 model = load_model('models\\model.h5')
-data = stock('AAPL', '..\\..\\..\\project\\main\\stock\\historical_data\\').get_data()
+data = stock('A', '..\\..\\..\\project\\main\\stock\\historical_data\\').get_data()
 test_data = np.array([[], []])
 
-opens = normalize_data(data['Open'][-21:-1].to_numpy())
-highs = normalize_data(data['High'][-21:-1].to_numpy())
-adj_closes = normalize_data(data['Adj Close'][-21:-1].to_numpy(), save_scaler=True)
+scaler1 = joblib.load('models\\scalers\\y_scaler.gz')
+scaler2 = joblib.load('models\\scalers\\x0_scaler.gz')
+scaler3 = joblib.load('models\\scalers\\x1_scaler.gz')
+
+opens = scaler2.transform((data['Open'][-100:-80].to_numpy()).reshape(-1,1)).flatten()
+highs = scaler3.transform((data['High'][-100:-80].to_numpy()).reshape(-1,1)).flatten()
+adj_closes = data['Adj Close'][-99:-79].to_numpy()
+ac = scaler1.transform((adj_closes).reshape(-1,1)).flatten()
 
 test_data = np.concatenate((test_data, np.stack([opens, highs])), axis=1)
 test_data = np.reshape(test_data.T, (1, 20, 2))
 print(test_data.shape)
 
 prediction = model.predict(test_data, verbose = 0)
-scaler = joblib.load('scaler.gz')
-print(adj_closes)
-print(scaler.inverse_transform(adj_closes.reshape(-1,1)))
+
+
+#print(scaler1.inverse_transform(adj_closes.reshape(-1,1)))
+print(ac)
 print(prediction)
-print(scaler.inverse_transform(prediction))
+print(adj_closes)
+print(scaler1.inverse_transform(prediction))
